f77a4e31c26faa32bfb849d7086be3a0fbfc3300
EladBarkay <eladb1231@gmail.com>
2022-01-17 10:00:43 +0200

    NEW DATA ON RAW DATA - ignored filed. Now can ignore filed name if not needed

--- src/main/java/workspace/michlala/HDHM/RawData.java
+++ src/main/java/workspace/michlala/HDHM/RawData.java
@@ -1,17 +1,18 @@
 package workspace.michlala.HDHM;
 
+import javax.naming.NamingException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 public class RawData {
+    final String IGNORE = "IGNORE";
     private HashMap<String, Object> variables;
-
-    public RawData(HashMap<String, Object> variables) {
-        this.variables = variables;
-    }
+    int ignoredCount;
 
     public RawData() {
         this.variables = new HashMap<>();
+        this.ignoredCount = 0;
     }
 
     public HashMap<String, Object> getVariables() {
@@ -22,7 +23,27 @@ public class RawData {
         this.variables = variables;
     }
 
-    public Object put(String name, Object value) {
+    private int getIgnoredCount() {
+        return ignoredCount;
+    }
+
+    private void setIgnoredCount(int ignoredCount) {
+        this.ignoredCount = ignoredCount;
+    }
+
+    private String getIgnoreName() {
+        return IGNORE + getIgnoredCount();
+    }
+
+    public Object putWithIgnoredName(Object value) {
+        Object toRet = this.variables.put(getIgnoreName(), value);
+        this.ignoredCount++;
+        return toRet;
+    }
+
+    public Object put(String name, Object value) throws NamingException {
+        if (name.startsWith(IGNORE))
+            throw new NamingException("CANT CREATE VARIABLE WITH NAME START WITH " + IGNORE);
         return this.variables.put(name, value);
     }
 
@@ -44,5 +65,23 @@ public class RawData {
 
     public Set<String> keySet() {
         return this.variables.keySet();
+    }
+
+    public Set<String> ignoredKeySet() {
+        Set<String> toRet = new HashSet<>();
+        for (String key : keySet()) {
+            if (key.startsWith(IGNORE))
+                toRet.add(key);
+        }
+        return toRet;
+    }
+
+    public Set<String> namedKeySet() {
+        Set<String> toRet = new HashSet<>();
+        for (String key : keySet()) {
+            if (!key.startsWith(IGNORE))
+                toRet.add(key);
+        }
+        return toRet;
     }
 }
