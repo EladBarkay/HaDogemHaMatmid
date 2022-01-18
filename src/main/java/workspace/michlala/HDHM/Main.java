d183b0b2151f6dbc5cb18705a92f333b36c0e075
EladBarkay <eladb1231@gmail.com>
2022-01-18 14:33:33 +0200

    New main to test the results

--- src/main/java/workspace/michlala/HDHM/Main.java
+++ src/main/java/workspace/michlala/HDHM/Main.java
@@ -4,6 +4,7 @@ import workspace.michlala.HDHM.ETLs.ETL;
 import workspace.michlala.HDHM.Extractors.CSV.CSV_Extractor;
 import workspace.michlala.HDHM.Extractors.CSV.CSV_Extractor_common_csv;
 import workspace.michlala.HDHM.Loaders.JSON.JSON_Loader;
+import workspace.michlala.HDHM.Transformers.AddDataToLabTest;
 
 import javax.naming.NoInitialContextException;
 import java.io.IOException;
@@ -12,9 +13,11 @@ public class Main {
     public static void main(String[] args) {
         ETL test = new ETL();
         CSV_Extractor_common_csv csv_extractor = new CSV_Extractor_common_csv("src/main/resources/MadaReports.csv");
+        AddDataToLabTest addDataToLabTest = new AddDataToLabTest();
         JSON_Loader json_loader = new JSON_Loader("src/main/resources/MadaReports.json");
 
         test.setExtractor(csv_extractor);
+        test.addTransformer(addDataToLabTest);
         test.setLoader(json_loader);
 
         try {
