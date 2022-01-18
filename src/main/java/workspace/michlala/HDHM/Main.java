9fbc2e1d20edca02513ad0aec6dd8fbeab30d2d6
EladBarkay <eladb1231@gmail.com>
2022-01-18 09:12:32 +0200

    Adjusting main

--- src/main/java/workspace/michlala/HDHM/Main.java
+++ src/main/java/workspace/michlala/HDHM/Main.java
@@ -2,20 +2,17 @@ package workspace.michlala.HDHM;
 
 import workspace.michlala.HDHM.ETLs.ETL;
 import workspace.michlala.HDHM.Extractors.CSV.CSV_Extractor;
+import workspace.michlala.HDHM.Extractors.CSV.CSV_Extractor_common_csv;
 import workspace.michlala.HDHM.Loaders.JSON.JSON_Loader;
-import workspace.michlala.HDHM.Transformers.NoChangesTransformer;
-import workspace.michlala.HDHM.Transformers.TransformAble;
 
 import javax.naming.NoInitialContextException;
 import java.io.IOException;
-import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         ETL test = new ETL();
-        CSV_Extractor csv_extractor = new CSV_Extractor(); csv_extractor.setPath("src/main/resources/MadaReports.csv");
-        NoChangesTransformer transformer = new NoChangesTransformer();
-        JSON_Loader json_loader = new JSON_Loader(); json_loader.setPath("src/main/resources/MadaReports.json");
+        CSV_Extractor_common_csv csv_extractor = new CSV_Extractor_common_csv("src/main/resources/MadaReports.csv");
+        JSON_Loader json_loader = new JSON_Loader("src/main/resources/MadaReports.json");
 
         test.setExtractor(csv_extractor);
         test.setLoader(json_loader);
