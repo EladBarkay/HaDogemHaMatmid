01608e4e2cfd3acc573d45d806579767583d43d5
EladBarkay <eladb1231@gmail.com>
2022-01-18 13:49:25 +0200

    Ctrl Alt L on JSON_Loader and removing things i dont need

--- src/main/java/workspace/michlala/HDHM/Loaders/JSON/JSON_Loader.java
+++ src/main/java/workspace/michlala/HDHM/Loaders/JSON/JSON_Loader.java
@@ -2,7 +2,6 @@ package workspace.michlala.HDHM.Loaders.JSON;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import workspace.michlala.HDHM.Loaders.Loader;
 
 import java.io.*;
@@ -18,7 +17,7 @@ public class JSON_Loader extends Loader {
         super(settings);
     }
 
-    public JSON_Loader(String path){
+    public JSON_Loader(String path) {
         super();
         setPath(path);
     }
@@ -31,58 +30,46 @@ public class JSON_Loader extends Loader {
         return new FileWriter(this.mostRelevantPath);
     }
 
-    public void setPath(String path){
-        setSettings(new HashMap<>(){{put("path", path);}});
+    public void setPath(String path) {
+        setSettings(new HashMap<>() {{
+            put("path", path);
+        }});
         mostRelevantPath = getRelevantPath();
     }
 
-    public String getPath(){
+    public String getPath() {
         return (String) getSettings().get("path");
     }
 
-    private String getRelevantPath(){
+    private String getRelevantPath() {
         String absPath = getPath();
-        if (getPath().toLowerCase(Locale.ROOT).endsWith(JSON_TYPE)){
-            absPath = getPath().substring(0, getPath().length()-5);
+        if (getPath().toLowerCase(Locale.ROOT).endsWith(JSON_TYPE)) {
+            absPath = getPath().substring(0, getPath().length() - 5);
         }
         int counter = 0;
         String finalAbsPath = absPath;
-        while (new File(finalAbsPath + JSON_TYPE).exists()){
+        while (new File(finalAbsPath + JSON_TYPE).exists()) {
             finalAbsPath = absPath + "(" + counter + ")";
             counter++;
         }
         return finalAbsPath + JSON_TYPE;
     }
 
-    private int fileLines(String path) {
-        int counter = 0;
-        try {
-            Scanner s = new Scanner(new File(path));
-            while (s.hasNextLine()){
-                s.nextLine();
-                counter++;
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return counter;
-    }
-
-
 
     @Override
     public void load(Properties data) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         ArrayList<Properties> dividedData = getDividedData(data);
-        for (Properties subData : dividedData){
-            for (Object key : subData.keySet()){
-                if (key == "rows"){
+        for (Properties subData : dividedData) {
+            for (Object key : subData.keySet()) {
+                if (key == "rows") {
                     mapper.writerWithDefaultPrettyPrinter().writeValue(getFileWriter(), subData.get("rows"));
                 }
             }
         }
     }
 
+    @SuppressWarnings("unchecked")
     private ArrayList<Properties> getDividedData(Properties data) {
         ObjectMapper mapper = new ObjectMapper();
 
@@ -91,14 +78,14 @@ public class JSON_Loader extends Loader {
         Properties tempProperty = new Properties();
         ArrayList<Properties> subProperties;
 
-        if (data.keySet().size() == 1){
+        if (data.keySet().size() == 1) {
             Object key = data.keySet().iterator().next();
-            if (data.get(key) instanceof Collection<?>){
+            if (data.get(key) instanceof Collection<?>) {
                 ArrayList<Properties> subData = new ArrayList<>((Collection<? extends Properties>) data.get(key));
                 tempProperty = new Properties();
                 subProperties = new ArrayList<>();
                 tempProperty.put(key, subProperties);
-                for (Properties property : subData){
+                for (Properties property : subData) {
                     subProperties.add(property);
                     String toPrint = "";
                     try {
@@ -106,8 +93,8 @@ public class JSON_Loader extends Loader {
                     } catch (JsonProcessingException e) {
                         e.printStackTrace();
                     }
-                    if (countLines(toPrint) > MAX_LINES){
-                        subProperties.remove(subProperties.size()-1);
+                    if (countLines(toPrint) > MAX_LINES) {
+                        subProperties.remove(subProperties.size() - 1);
                         toRet.add((Properties) tempProperty.clone());
                         subProperties = new ArrayList<>();
                         subProperties.add(property);
@@ -121,8 +108,8 @@ public class JSON_Loader extends Loader {
         return toRet;
     }
 
-    private static int countLines(String str){
+    private static int countLines(String str) {
         String[] lines = str.split("\r\n|\r|\n");
-        return  lines.length;
+        return lines.length;
     }
 }
