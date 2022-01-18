76fbcba893307f19c480795e6e329c071ae38fd0
EladBarkay <eladb1231@gmail.com>
2022-01-18 09:02:26 +0200

    New CSV Extractor - working with CSVParser, CSVRecords etc so should work better..

---
+++ src/main/java/workspace/michlala/HDHM/Extractors/CSV/CSV_Extractor_2.java
@@ -0,0 +1,68 @@
+package workspace.michlala.HDHM.Extractors.CSV;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import workspace.michlala.HDHM.Extractors.Extractor;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class CSV_Extractor_2 extends Extractor {
+
+    public CSV_Extractor_2(HashMap<String, Object> settings) {
+        super(settings);
+    }
+
+    public CSV_Extractor_2() {
+    }
+
+    public void setPath(String path){
+        setSettings(new HashMap<>(){{put("path", path);}});
+    }
+
+    public String getPath(){
+        return (String) getSettings().get("path");
+    }
+
+    @Override
+    public Properties extract() throws IOException {
+        Properties properties;
+        String path = (String) getSettings().get("path");
+        ArrayList<CSVRecord> CSVData;
+        try {
+            CSVData = getRecords(path);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.print("error 0_0");
+            throw e;
+        }
+        properties = CSVToProperties(CSVData);
+        return properties;
+    }
+
+    private Properties CSVToProperties(ArrayList<CSVRecord> csvData){
+        Properties allData = new Properties();
+        ArrayList<Properties> addHere = new ArrayList<>();
+        for (CSVRecord record : csvData){
+            Properties tempProperty = new Properties();
+            tempProperty.putAll(record.toMap());
+            addHere.add(tempProperty);
+        }
+        allData.put("rows", addHere);
+        return allData;
+    }
+
+    public ArrayList<CSVRecord> getRecords(String path) throws IOException {
+        CSVParser parser = new CSVParser(new FileReader(path), CSVFormat.DEFAULT.withHeader());
+        ArrayList<CSVRecord> toRet = new ArrayList<>();
+        parser.forEach(toRet::add);
+        return toRet;
+    }
+}
+
