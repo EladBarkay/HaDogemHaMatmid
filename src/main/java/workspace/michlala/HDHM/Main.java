f0d56280c39afcb7fe43733615faa609c1df3847
EladBarkay <eladb1231@gmail.com>
2022-01-18 17:12:43 +0200

    Now can save the data as xml!

--- src/main/java/workspace/michlala/HDHM/Main.java
+++ src/main/java/workspace/michlala/HDHM/Main.java
@@ -1,9 +1,9 @@
 package workspace.michlala.HDHM;
 
 import workspace.michlala.HDHM.ETLs.ETL;
-import workspace.michlala.HDHM.Extractors.CSV.CSV_Extractor;
 import workspace.michlala.HDHM.Extractors.CSV.CSV_Extractor_common_csv;
-import workspace.michlala.HDHM.Loaders.JSON.JSON_Loader;
+import workspace.michlala.HDHM.Loaders.FileLoaders.JSON.JSON_Loader;
+import workspace.michlala.HDHM.Loaders.FileLoaders.XML.XML_Loader;
 import workspace.michlala.HDHM.Transformers.AddDataToLabTest;
 
 import javax.naming.NoInitialContextException;
@@ -14,11 +14,11 @@ public class Main {
         ETL test = new ETL();
         CSV_Extractor_common_csv csv_extractor = new CSV_Extractor_common_csv("src/main/resources/LabTests.csv");
         AddDataToLabTest addDataToLabTest = new AddDataToLabTest();
-        JSON_Loader json_loader = new JSON_Loader("src/main/resources/results.json");
-
+        JSON_Loader json_loader = new JSON_Loader("src/main/resources/results.json", ".json", 50000);
+        XML_Loader xml_loader = new XML_Loader("src/main/resources/results.xml", ".xml", 50000);
         test.setExtractor(csv_extractor);
         test.addTransformer(addDataToLabTest);
-        test.setLoader(json_loader);
+        test.setLoader(xml_loader);
 
         try {
             test.start();
